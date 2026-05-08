e17750f8a5861e462926cda3ae81e7f3b3058846
aris <aris123muslim@gmail.com>
2026-05-08 19:12:52 +0700

    boipler project

---
+++ main.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("button");
+const container = document.querySelector(".container");
+const judul = document.getElementById("judul");
+
+
+button.addEventListener("click", () => {
+    judul.style.display = "block";
+});
