d31c162901be88163f17d4396ec7bd21fc79727c
aris <aris123muslim@gmail.com>
2026-05-17 20:11:39 +0700

    menambahkan fitur toogle background

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const button = document.getElementById("button");
 const container = document.querySelector(".container");
 const judul = document.getElementById("judul");
+const background = document.getElementById("background")
+const toggleButton = document.getElementById("toggle")
 
 
 button.addEventListener("click", () => {
     judul.style.display = "block";
 });
+
+
+toggleButton.addEventListener("click", () => {
+    background.classList.toggle("toggleBackground")
+})
